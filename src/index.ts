aeff432b57e04151f9ef6004ed02e572f850f6fa
Edwin Realpe <edwin@ventures-sas.com>
2026-05-08 02:10:28 -0500

    chore: remove legacy commands to focus on core protocol

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
+#!/usr/bin/env node
 import { Command } from 'commander'
 import { initCommand } from './commands/init.js'
 import { configCommand } from './commands/config.js'
 import { contextCommand } from './commands/context.js'
-import fs from 'fs'
+import fs from 'fs-extra'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Leer versión desde package.json
-const pkgPath = new URL('../package.json', import.meta.url)
-const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
-const { version } = pkg
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8'))
 
 const program = new Command()
 
 program
   .name('nexus')
-  .description('NEXUS: El protocolo de comunicación minimalista Humano-IA')
-  .version(version)
+  .description('NEXUS: Human-AI Language Protocol CLI')
+  .version(packageJson.version)
 
+// Solo registramos los comandos esenciales del protocolo
 program.addCommand(initCommand())
 program.addCommand(configCommand())
 program.addCommand(contextCommand())
 
-program.parse()
+program.parse(process.argv)
